6c0187d93d1246307780d4b9f585fef06b607c7f
Даниэль Носков <daniel.noskov.99@mail.ru>
2023-01-03 14:01:23 +0300

    Добавлены новые возможности в приложение Posts

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from . import views
 
+
+app_name = 'posts'
 urlpatterns = [
     # Главная страница
-    path('',views.index),
+    path('',views.index, name='main'),
     # Страница с группами
-    path('group/<slug:any_slug>/', views.group_posts),
+    # path('group/<slug:any_slug>/', views.group_posts),
+    path('groups/', views.group_posts, name='groups')
 ]
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
+from .models import Post
 
 
 # Create your views here.
 # Главная страница
 def index(request):
-    return HttpResponse('Главная страница Yatube.ru')
+    posts = Post.objects.order_by('-pub_date')[:10]
+    context = {'posts':posts}
+    return render(request, 'posts/index.html', context)
 
 
 # Страница с группами
-def group_posts(request, any_slug):
-    return HttpResponse(f'Страница группы с постом {any_slug}')
+def group_posts(request):
+    template = 'posts/group_list.html'
+    title = "Здесь будет информация о группах проекта Yatube"
+    context = {'title' : title}
+    return render(request, template, context)
